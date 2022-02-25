2543e103b9e25911e742def487a2c7fcd9b66b71
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-02-25 11:13:43 +1100

    API Implmentation 01

--- api/config.php
+++ api/config.php
@@ -4,5 +4,18 @@
 header('content-type: application/json');
 echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
 
+$_mo =  $_POST['mobile']; 
+$_email = $_POST['email'];
+
+if ($_mo == "" || $_email == "") { 
+	echo "The mobile number or email was empty, please try again";
+	header('./index.html');
+	exit; 
+} else { 
+	echo $_m; 
+	echo " "; 
+	echo $_email;
+
+}
 
 ?>
