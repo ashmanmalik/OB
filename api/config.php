c940f2e39b1542e84be21226be71271ad5e95a27
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-02-25 11:23:13 +1100

    API Implmentation 04 - Token EP

--- api/config.php
+++ api/config.php
@@ -6,5 +6,26 @@ echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']
 
 var_dump($_POST);
 
+$url = "https://au-api.basiq.io/token";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Content-Type: application/x-www-form-urlencoded",
+   "basiq-version: 2.1",
+   "Authorization: Basic OTgzMGZhNmMtYjIyMi00ZDg2LWJhMjYtNjkyZjEwNDI2ZjQ4OmJhN2ZmMzNlLTBiYmUtNGEzMC1iMWUzLWFiNGJjODQ4ZTczNg==",
+   "Content-Length: 0",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
 
 ?>
