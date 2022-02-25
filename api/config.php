514aee48b0463e3f573e45fd3ca0faabfa364795
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-02-25 14:48:41 +1100

    User API added v2

--- api/config.php
+++ api/config.php
@@ -88,7 +88,7 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $headers = array(
-   "Authorization: Bearer '$server_obj->access_token'",
+   "Authorization: Bearer ".$server_obj->access_token."",
    "Accept: application/json",
    "Content-Type: application/json",
 );
