78a8ea8619bf638d04abd94d61a5883a3708f7f0
Ashman Malik <ashman@thinkun.com>
2022-02-25 20:44:40 +1100

    Testing redirect for Session storage

--- api/config.php
+++ api/config.php
@@ -102,7 +102,31 @@ $client_obj = json_decode( $client_token );
 echo "client token : ".$client_obj->access_token;
 
 
-// Test
+// Storing the values in DB
+
+$db = new SQLite3('/tmp/db.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+
+$db->query('CREATE TABLE IF NOT EXISTS "cipher" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "user" VARCHAR,
+    "token" VARCHAR, 
+    "time" DATETIME
+)');
+
+$statement = $db->prepare('INSERT INTO "cipher" ("user", "token", "time") VALUES ('.$user_object->id;.','$server_obj->token', :time)');
+//$statement->bindValue(':url', ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'https') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+$statement->bindValue(':time', date('Y-m-d H:i:s'));
+$statement->execute();
+
+$visits = $db->querySingle('SELECT user, token FROM "cipher"');
+
+var_dump($visits);
+echo("value: $visits");
+
+$db->close();
+
+// End Database ..
+
 
 $redirect_url = 'https://consent.basiq.io/home?userId='.$user_object->id.'&token='.$client_obj->access_token; 
 
