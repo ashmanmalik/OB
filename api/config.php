b2e2eb3f67aa33c32c05c36f00550b962ba60745
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-02-25 15:55:36 +1100

    User API added v2.5

--- api/config.php
+++ api/config.php
@@ -84,33 +84,32 @@ echo "\n";
 
 // Calling token for Client_access EP to use it into Consent. 
 
-// $url = "https://au-api.basiq.io/token";
+$url = "https://au-api.basiq.io/token";
 
-// $curl = curl_init($url);
-// curl_setopt($curl, CURLOPT_URL, $url);
-// curl_setopt($curl, CURLOPT_POST, true);
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-// $headers = array(
-//    "Content-Type: application/x-www-form-urlencoded",
-//    "basiq-version: 3.0",
-//    "Authorization: Basic NzM4NDE4YjktNDdlYy00OGI2LTg5ODEtNjg0OGI3NzU2ZDczOmQ4OTVlZmUzLTcwOTMtNDdlZC1hN2NmLTVmMWFkNGJlY2MwOA==",
-//    "Content-Length: 0",
-// );
-// curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-// $data = '{"scope": "CLIENT_ACCESS"}';
-// curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-// //for debug only!
-// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$headers = array(
+   "Content-Type: application/x-www-form-urlencoded",
+   "basiq-version: 3.0",
+   "Authorization: Basic NzM4NDE4YjktNDdlYy00OGI2LTg5ODEtNjg0OGI3NzU2ZDczOmQ4OTVlZmUzLTcwOTMtNDdlZC1hN2NmLTVmMWFkNGJlY2MwOA==",
+   "Content-Length: 0",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$data = '{"scope": "CLIENT_ACCESS"}';
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-// $client_token = curl_exec($curl);
-// curl_close($curl);
+$client_token = curl_exec($curl);
+curl_close($curl);
 
-// $client_obj = json_decode( $server_token );
+$client_obj = json_decode( $server_token );
 
-
-// echo "client token : ".$client_obj->access_token;
+echo "client token : ".$client_obj->access_token;
 
 // echo "\n";
 // echo "\n";
@@ -118,7 +117,6 @@ echo "\n";
 // echo "\n";
 
 
-// ... 
 
 
 // .. Redirecting the user to Consent.basiq.io 
