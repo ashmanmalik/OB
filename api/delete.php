16e224e85c01e26dc76ecbb0aa3b13196aafff6a
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-03-02 16:44:37 +1100

    Delete users magic

--- api/delete.php
+++ api/delete.php
@@ -9,21 +9,22 @@ $_token = $_GET["token"];
 
 <script type="text/javascript">
 
-var userID = <?php echo $u_ID; ?>; 
-var token = <?php echo $_token; ?>;
+var userID = <?php echo json_encode($u_ID); ?>; 
+var token = <?php echo json_encode($_token); ?>;
 
 	var url = "https://au-api.basiq.io/users/"+userID+"";
 
 var xhr = new XMLHttpRequest();
 xhr.open("DELETE", url);
 
-xhr.setRequestHeader("Authorization", "Bearer "+token);
+xhr.setRequestHeader("Authorization", "Bearer "+token+"");
 
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log(xhr.status);
       if (xhr.status == 204)  { 
       	window.location.href = "https://ob-omega.vercel.app/api/admin_dashboard.php";
+      	console.log("user deleted");
       }
       else { 
       	console.log("user already deleted");
