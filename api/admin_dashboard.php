0291215a6fc34cd9b7b5ab49531c0643d2cddf94
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-03-02 16:50:17 +1100

    Delete users magic 01

--- api/admin_dashboard.php
+++ api/admin_dashboard.php
@@ -76,6 +76,7 @@ $users_bucket = $myObject["data"];
     var table = $('#example').DataTable({
         dom: 'Pfrtip',
     });
+    table.ajax.reload();
  
     // Create the chart with initial data
     var container = $('<div/>').insertBefore(table.table().container());
@@ -164,30 +165,5 @@ function chartData(table) {
         </tfoot>
     </table>
   </div>
-<script>
-
-  $(function() {
-    $('#myTable').bootstrapTable()
-  })
-
-function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
-</script>
 </body>
 </html>
