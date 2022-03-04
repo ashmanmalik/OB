522e134ca31c590d2543541a5739f950381c1d57
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-03-04 11:25:34 +1100

    Add Client token

--- api/config.php
+++ api/config.php
@@ -80,7 +80,7 @@ $headers0 = array(
 );
 curl_setopt($curl0, CURLOPT_HTTPHEADER, $headers0);
 
- $data0 = '{"scope": "CLIENT_ACCESS", "userId": '.json_encode($user_object->id).'';
+ $data0 = '{"scope": "CLIENT_ACCESS", "userId": "$user_object->id)"';
 
 echo $data0;
 var_dump($data0);
