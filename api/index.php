d91d5b86e1a7f209abd0dd47ac48df53a175c87c
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-03-07 16:04:56 +1100

    Index added with Marker Locations

--- api/index.php
+++ api/index.php
@@ -26,15 +26,13 @@ $accounts = json_decode( $resp );
 	$enrich_data = $myObject["data"]; 
 	for ($i=0; $i<500; $i++) {
 	  if ($enrich_data[$i]["enrich"]["location"]) { 
-	    if($enrich_data[$i]["enrich"]["location"]["geometry"] != "") {
-
-	    $lat = $enrich_data[$i]["enrich"]["location"]["geometry"]["lat"];
-	    $lon = $enrich_data[$i]["enrich"]["location"]["geometry"]["lng"];
-
-	      $planes[] = $lat;
-		  $planes[] = $lon;
-
-
+	    if($enrich_data[$i]["enrich"]["location"]["geometry"]) {
+	    	if (($enrich_data[$i]["enrich"]["location"]["geometry"]["lat"] != "") && ($enrich_data[$i]["enrich"]["location"]["geometry"]["lon"] != "")) { 
+			    $lat = $enrich_data[$i]["enrich"]["location"]["geometry"]["lat"];
+			    $lon = $enrich_data[$i]["enrich"]["location"]["geometry"]["lng"];
+		      	$planes[] = $lat;
+			  	$planes[] = $lon;
+		   	}
 	    }
 	  }
 	}
