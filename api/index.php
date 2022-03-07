6c1071f3b3cdb35c642b614866cae18f881ed417
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-03-07 16:30:01 +1100

    Test 04

--- api/index.php
+++ api/index.php
@@ -23,7 +23,6 @@ $accounts = json_decode( $resp );
 	$myObject = json_decode($resp, true);
 	$transaction_data = $myObject["data"];
 	$planes = array();
-	$planer = array();
 	$enrich_data = $myObject["data"]; 
 	for ($i=0; $i<500; $i++) {
 	  if ($enrich_data[$i]["enrich"]["location"]) { 
@@ -33,13 +32,13 @@ $accounts = json_decode( $resp );
 		    	$lon = $enrich_data[$i]["enrich"]["location"]["geometry"]["lng"];
 		      	$planes[] = $lat;
 			  	$planes[] = $lon;
-			  	$planer[] = $planes[];
 	    	}
 	    }
+
 	  }
+	  $planer = array($planes);
 	}
 	echo json_encode($planer);
-	echo json_encode(array($planes));
 ?>
 <!DOCTYPE html>
 <html>
