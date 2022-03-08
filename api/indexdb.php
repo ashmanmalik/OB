fab632d98cabd5c447ea6e2fbe9aa20b1df592e1
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-03-08 11:39:27 +1100

    Enrich URL fixed

--- api/indexdb.php
+++ api/indexdb.php
@@ -1,6 +1,8 @@
 
 <?php 
 
+
+
 $url = "https://au-api.basiq.io/users/".$_GET['user']."/transactions?filter=account.id.eq(".$_GET['accountno'].")";
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
@@ -19,103 +21,94 @@ curl_close($curl);
 
 $accounts = json_decode( $resp );
 
-// //Print the array in a simple JSON format
-// echo '<pre>';
-// echo json_encode($accounts, JSON_PRETTY_PRINT);
-// echo '</pre>';
 
+	$myObject = json_decode($resp, true);
+	$transaction_data = $myObject["data"];
+	$enrich_data = $myObject["data"]; 
+	for ($i=0; $i<500; $i++) {
+	  if ($enrich_data[$i]["enrich"]["location"]) { 
+	   //  if($enrich_data[$i]["enrich"]["location"]["geometry"]) {
+	   //  	if ($enrich_data[$i]["enrich"]["location"]["geometry"]["lat"] != "" && $enrich_data[$i]["enrich"]["location"]["geometry"]["lng"] != "") { 
+				// $lat = $enrich_data[$i]["enrich"]["location"]["geometry"]["lat"];
+		  //   	$lon = $enrich_data[$i]["enrich"]["location"]["geometry"]["lng"];
+		  //   	$planes[] = array("test", $lat, $lon);
+		      	
 
+	   //  	}
+	   //  }
+	  	echo json_encode($enrich_data[$i]["enrich"]["location"]);
+
+	  }
+
+	}
+	//echo json_encode($planes);
+	$enriched_url = "indexdb.php?accountno=".$_GET['accountno']."&user=".$_GET['user']."&token=".$_GET["token"];
+	exit;
 ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title> Mapper </title>
-	<!-- Bagian css -->
-	<link rel="stylesheet" href="https://ashmanmalik.github.io/portifolio/assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://ashmanmalik.github.io/portifolio/assets/css/ilmudetil.css">
-	
-	<!-- Bagian js -->
-	<script src='https://ashmanmalik.github.io/portifolio/assets/js/jquery-1.10.1.min.js'></script>       
-    
-	<script src="https://ashmanmalik.github.io/portifolio/assets/js/bootstrap.min.js"></script>
-	<!-- akhir dari Bagian js -->
-	<script src="https://maps.google.com/maps/api/js?sensor=false"></script>
-	
-	
-	
+    <title> Simple Map </title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="https://d19vzq90twjlae.cloudfront.net/leaflet-0.7/leaflet.css" />
+    <style type="text/css">
+    	#refreshButton {
+		  position: absolute;
+		  top: 20px;
+		  right: 20px;
+		  padding: 10px;
+		  z-index: 400;
+		}
+    </style>
 </head>
-<body onload="initialize()">
-<div class="container" style="margin-top:10px">	
-	<div class="row">
-		<div class="col-md-8">
-			<div class="panel panel-default">
-					<div class="panel-body">
-						<div id="map-canvas" style="width: 700px; height: 600px;"></div>
-					</div>
-			</div>
-		</div>	
-	</div>
-</div>	
+<body>
 
-<script>
-		
-    var marker;
-      function initialize() {
-		  
-		var infoWindow = new google.maps.InfoWindow;
-		
-		var mapOptions = {
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        } 
-		
-		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-              
-        var bounds = new google.maps.LatLngBounds();
+    <div id="map" style=" height: 1000px"></div>
+    <a id="refreshButton" href="<?php echo $enriched_url; ?>"> Convert Addresses to Lat Long </a>
 
-		<?php
+    <script src="https://d19vzq90twjlae.cloudfront.net/leaflet-0.7/leaflet.js">
+    </script>
 
-			$myObject = json_decode($resp, true);
-			$transaction_data = $myObject["data"];
+    <script>
+    var planes = <?php echo json_encode($planes); ?>; 
+	
+    // //var planes = [
+    // ["abc", "-33.899629","151.0656538"],
+    // ["xyz", "-28.0291428","153.4337534"],
+    // ]; 
+	
+	// //var planes = [
+	// 	["7C6B07",-40.99497,174.50808],
+	// 	["7C6B38",-41.30269,173.63696],
+	// 	["7C6CA1",-41.49413,173.5421],
+	// 	["7C6CA2",-40.98585,174.50659],
+	// 	["C81D9D",-40.93163,173.81726],
+	// 	["C82009",-41.5183,174.78081],
+	// 	["C82081",-41.42079,173.5783],
+	// 	["C820AB",-42.08414,173.96632],
+	// 	["C820B6",-41.51285,173.53274]
+	// 	];
 
-			$enrich_data = $myObject["data"]; 
-			for ($i=0; $i<500; $i++) {
-			  if ($enrich_data[$i]["enrich"]["location"]) { 
-			    if($enrich_data[$i]["enrich"]["location"]["geometry"] != "") {
+        var map = L.map('map').setView([-33.899629, 151.0656538], 8);
+        mapLink = 
+            '<a href="https://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer(
+            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; ' + mapLink + ' Contributors',
+            maxZoom: 18,
+            }).addTo(map);
 
-			    $lat = $enrich_data[$i]["enrich"]["location"]["geometry"]["lat"];
-			    $lon = $enrich_data[$i]["enrich"]["location"]["geometry"]["lng"];
+		for (var i = 0; i < planes.length; i++) {
+			marker = new L.marker([planes[i][1],planes[i][2]])
+				.bindPopup(planes[i][0])
+				.addTo(map);
+		}
+// require_once __DIR__ . '/../vendor/autoload.php';
 
-			    echo ("addMarker($lat, $lon, '<b> test </b>');\n");
-			    }
-			  }
-			}
-          ?>
-		  
-		function addMarker(lat, lng, info) {
-			var lokasi = new google.maps.LatLng(lat, lng);
-			bounds.extend(lokasi);
-			var marker = new google.maps.Marker({
-				map: map,
-				position: lokasi
-			});       
-			map.fitBounds(bounds);
-			bindInfoWindow(marker, map, infoWindow, info);
-		 }
-		
-		function bindInfoWindow(marker, map, infoWindow, html) {
-          google.maps.event.addListener(marker, 'click', function() {
-            infoWindow.setContent(html);
-            infoWindow.open(map, marker);
-          });
-        }
- 
-        }
-      google.maps.event.addDomListener(window, 'load', initialize);
-    
-	</script>
+// Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
+
+// throw new RuntimeException('Hello Tracy!');
+               
+    </script>
 </body>
 </html>
