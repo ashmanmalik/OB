d2cd018aca4b29c70419451190f7caf06059eb3f
Ashman <ashman@192-168-1-108.tpgi.com.au>
2022-03-08 17:25:41 +1100

    Enriching data and converting address to lat lon

--- api/indexdb.php
+++ api/indexdb.php
@@ -13,8 +13,29 @@ echo '</pre>';
 
 echo '<p>&nbsp;</p>';
 echo json_encode($data[0]->location_formatted_address);
-echo '<p>&nbsp;</p>';
-echo json_encode($data[0]["location_formatted_address"]);
+// Now convert this to lat & lon
+
+$url = "http://maps.google.com/maps/api/geocode/json?address=".urlencode($data[0]->location_formatted_address);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);    
+$responseJson = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($responseJson);
+
+if ($response->status == 'OK') {
+    $latitude = $response->results[0]->geometry->location->lat;
+    $longitude = $response->results[0]->geometry->location->lng;
+
+    echo 'Latitude: ' . $latitude;
+    echo '<br />';
+    echo 'Longitude: ' . $longitude;
+} else {
+    echo $response->status;
+    var_dump($response);
+}    
 
 exit();
 exit; 
