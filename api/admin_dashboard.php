f33589a5a6ee32533faca99189b6922777deb688
Ashman <ashman@An0nym0usRoot3xDr.local>
2022-03-17 12:33:45 +1100

    Delete Ajax

--- api/admin_dashboard.php
+++ api/admin_dashboard.php
@@ -193,8 +193,9 @@ function chartData(table) {
               <td><?PHP echo $item["email"]; ?></td>
               <td><?PHP echo $item["mobile"]; ?></td>
               <td><?PHP echo $item["createdTime"]; ?></td>
-              <td><a href="listaccounts.php?userId=<?php echo $item["id"]; ?>&token=<?php echo $server_obj->access_token ?>"> View accounts </a> | <a href="delete.php?userId=<?php echo $item["id"]; ?>&token=<?php echo $server_obj->access_token ?>"> Delete </a></td>
+              <td><a href="listaccounts.php?userId=<?php echo $item["id"]; ?>&token=<?php echo $server_obj->access_token ?>"> View accounts </a> | <a id="deletingdata"> Delete </a></td>
             </tr>
+
           <?php endforeach; ?>
         </tbody>
         <tfoot>
@@ -209,7 +210,17 @@ function chartData(table) {
     </table>
   </div>
 <script>
+  var uId = <?php echo $item["id"]; ?>;
+  var tkn = <?php echo $server_obj->access_token; ?>;
 
+$('#deletingdata').click(function(event){
+   event.preventDefault(); // prevent default behavior of link click
+   // now make an AJAX request to server_side_file.php by passing some data
+   $.post('delete.php', {"userId" : uId, "token": tkn}, function(response){
+      //now you've got `response` from server, play with it like
+      alert(response);
+   });
+});
 
 
 function myFunction() {
