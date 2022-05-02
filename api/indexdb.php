5a0ae20b4cde3daf5283a3c03e832e9cae1334e2
ashman malik <ashmanmalik@An0nym0usRoot3xDr.local>
2022-05-02 15:15:40 +1000

    Testing

--- api/indexdb.php
+++ api/indexdb.php
@@ -1,8 +1,40 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title> illion Bank Statement </title>
+  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-// Creating Affordability Report Like illion From our Affordability Endpoint
+<div class="container">
+  <h2> Account Summary </h2>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
-echo "Testing"; 
-
-
-?>
+</body>
+</html>
