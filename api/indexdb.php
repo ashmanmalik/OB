239d5b8c08e0fb830c74f6a7e77d0da87e5f8ca9
ashman malik <ashmanmalik@An0nym0usRoot3xDr.local>
2022-05-09 15:44:40 +1000

    Slack Hooks automation

--- api/indexdb.php
+++ api/indexdb.php
@@ -2,7 +2,30 @@
 
 // Take the Job and use it.. 
 
-echo "Testing"; 
+$url = "https://hooks.slack.com/services/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";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Content-type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+$data = '{"text":"Hello, World!"}';
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
+
 
 
 ?>
