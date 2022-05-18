895db9b889c28f8b71bb66046043472a3e66fe36
Ash <ashman.malik@outlook.com>
2022-05-18 14:44:40 +1000

    Job Checked

--- api/indexdb.php
+++ api/indexdb.php
@@ -3,24 +3,20 @@
 // Take the Job and use it.. 
 
 $job = $_GET['job'];
-echo $job;
+$token = $_GET['token'];
 
-$url = "https://hooks.slack.com/services/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";
+$url = "https://au-api.basiq.io/jobs/".$job;
+
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 $headers = array(
-   "Content-type: application/json",
+   "Authorization: Bearer ".$token,
+   "Accept: application/json",
 );
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-$data = '{"text":"Hello, World!"}';
-
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-
 //for debug only!
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -29,7 +25,6 @@ $resp = curl_exec($curl);
 curl_close($curl);
 var_dump($resp);
 
-// sometyhing
 
 
 ?>
